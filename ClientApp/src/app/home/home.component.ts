67bbb08bafbc64b79f14fdc07e66c65da927caf2
Owen McPartlan <omcpartlan@impactnetworking.com>
2019-11-15 14:32:53 -0600

    small fixes

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -4,6 +4,13 @@ import { DataService } from '../data.service';
 
 import { $httpService } from '../$http.service';
 
+//Right now 4200 is successfully calling "localhost:5000", but returns an error because of 'localhost'
+
+//Publish backend to IIS
+//https://docs.microsoft.com/en-us/visualstudio/deployment/tutorial-import-publish-settings-iis?view=vs-2019
+
+
+//RUN THIS "chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security" AND EVERYTHING WILL BE OKAY AGAIN
 
 @Component({
   selector: 'app-home',
