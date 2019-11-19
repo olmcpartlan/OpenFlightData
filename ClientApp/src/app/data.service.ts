910703a989a004c105d541a290f0ca7b07180886
Owen McPartlan <olmcpartlan@gmail.com>
2019-11-18 20:16:49 -0600

    Auto-populating airports when you search City

--- ClientApp/src/app/data.service.ts
+++ ClientApp/src/app/data.service.ts
@@ -1,28 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { HomeComponent } from './home/home.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  @Input() airports = [];
+
   constructor(private httpClient: HttpClient) { }
-  getBaseUrl() {
-    return 'http://192.168.0.4:5000/';
+
+  returnData() {
+    return this.airports;
   }
 
   sendGetRequest(location) {
     console.log(location.muni);
-    
-    return this.httpClient.get(`http://192.168.0.4:5000/${location.muni}`)
-        .map((response: Response) => {
-          console.log(response);
-        })
+
+    return this.httpClient.get(`http://localhost:5000/${location.muni}`)
+      .map((response: Response) => {
+        console.log("****************");
+        console.log(response);
+
+        for (var port in response) {
+          console.log(response[port].name);
+          this.airports.push(response[port].name);
+        }
         
-
-
+      })
   }
 
 }
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,19 +5,15 @@ import { DataService } from '../data.service';
 import { $httpService } from '../$http.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
-//Right now 4200 is successfully calling "localhost:5000", but returns an error because of 'localhost'
-
-//Publish backend to IIS
-//https://docs.microsoft.com/en-us/visualstudio/deployment/tutorial-import-publish-settings-iis?view=vs-2019
-
-
-//RUN THIS "chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security" AND EVERYTHING WILL BE OKAY AGAIN
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  providers: [DataService]
 })
+
+
 export class HomeComponent {
+  selectedAirports;
   form: FormGroup;
   constructor(private dataService: DataService, public fb: FormBuilder) {
     this.form = this.fb.group({
@@ -25,23 +21,16 @@ export class HomeComponent {
     })
   }
 
-  // ngOnInit() {
-  //   console.log("started");
-  //   this.dataService.sendGetRequest().subscribe(
-  //     () => console.log("loaded")
-  //   )
-    
-  // }
-  // public _sendGetRequest() {
-  //   this.dataService.sendGetRequest()
-  //     .subscribe((data) => {
-  //       console.log(data);
-  //     });
-  // }
+  displayVals() {
+    console.log("PLEASE")
+    var data = this.dataService.returnData();
+    console.log(data);
+    this.selectedAirports = data;
+  }
 
   public sendForm() {
-    console.log(this.form.value);
     this.dataService.sendGetRequest(this.form.value)
       .subscribe();
+    this.displayVals();
   }
 }
