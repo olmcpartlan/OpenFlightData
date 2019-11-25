0f38503fe905783ed0a64102cdfbc6af489e9c6a
Owen McPartlan <olmcpartlan@gmail.com>
2019-11-24 18:24:00 -0600

    Horizontal scroll for each day in 5 day forecast

--- ClientApp/src/app/display/display.component.ts
+++ ClientApp/src/app/display/display.component.ts
@@ -10,18 +10,30 @@ import { ResultService } from '../result.service';
   providers: [DataService],
   template: 
   `
-    <div class="forecast" *ngFor="let dir of directions">
-      <p>{{dir}}</p>
+  <div *ngFor="let row of list">
+    <div class="container">
+      <div class="forecast" *ngFor="let dir of row">
+        <p>{{dir}}</p>
+        <img class="image" src="assets/directions/{{dir}}.png" alt="err" />
+      </div>
     </div>
+  </div>
   `
 })
 export class DisplayComponent {
   directions;
+  list = [];
+  
   constructor(private dataService: DataService, private home: HomeComponent, private result: ResultService) { }
   
   public ngOnInit() {
     this.directions = this.result.directions;
-    
+    this.list.push(this.directions.slice(0,8));
+    this.list.push(this.directions.slice(8,16));
+    this.list.push(this.directions.slice(16,24));
+    this.list.push(this.directions.slice(24,32));
+    this.list.push(this.directions.slice(32,40));
+    console.log(this.list);
   }
 
   // getDirection(dir) {
